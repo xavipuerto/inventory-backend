58ff88a4e39271e7515fac0f75048629b0e94e16
Javier Aragon Diaz <jaaragon@mercadona.es>
2023-04-14 17:22:21 +0200

    feat: añadir CORS - Acceso de otras aplicaciones a backend

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+//el cors (CrossOrigin) es para poder hacer llamadas desde otras aplicaciones
+//el puerto 4200 es el que escucha angular por defecto
+@CrossOrigin (origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
